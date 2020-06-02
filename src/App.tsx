1b8a7de2f4f332d7219b458392e954471c03b822
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-01 20:43:12 -0700

    add button component, part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
-        <h2>Hello World</h2>
-        <h3>Hello World</h3>
-        <hr />
-        <code>
-          const a = 'b'
-        </code>
+        <Button disabled>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          Hello
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          href="https://www.google.com/"
+          disabled
+        >
+          Google Link
+        </Button>
 
         <p>
           Edit <code>src/App.tsx</code> and save to reload.

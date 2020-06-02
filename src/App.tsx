a6cf53ec9852e9a46931d2fce6ec7c4a60a08a69
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-01 21:11:19 -0700

    add button style, part 1

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button disabled>Hello</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
-          Hello
-        </Button>
-        <Button
-          btnType={ButtonType.Link}
-          href="https://www.google.com/"
-          disabled
-        >
-          Google Link
-        </Button>
-
+        <Button> Hello </Button>
+        <Button disabled> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.google.com"> Google Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.google.com" disabled> Disabled Link </Button>
+     
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>

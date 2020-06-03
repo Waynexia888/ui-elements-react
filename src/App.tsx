3993edcedc58c06f092989f8484a9861951fea1f
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-02 19:15:55 -0700

    Menu component coding part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";  
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0}>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem>cool link 2</MenuItem>
+          <MenuItem>cool link 3</MenuItem>
+        </Menu>
         <Button className="custom"> Hello </Button>
         <Button disabled> Disabled Button </Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
-        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
-        <Button btnType={ButtonType.Link} href="http://www.google.com" target="_blank"> Google Link </Button>
-        <Button btnType={ButtonType.Link} href="http://www.google.com" disabled> Disabled Link </Button>
-     
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          {" "}
+          Large Primary{" "}
+        </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+          {" "}
+          Small Danger{" "}
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          href="http://www.google.com"
+          target="_blank"
+        >
+          {" "}
+          Google Link{" "}
+        </Button>
+        <Button btnType={ButtonType.Link} href="http://www.google.com" disabled>
+          {" "}
+          Disabled Link{" "}
+        </Button>
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>

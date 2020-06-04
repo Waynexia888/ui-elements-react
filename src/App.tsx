df7c6bd104dd804524b1bb1f6e6590397fcdda9d
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-03 22:09:05 -0700

    use javascript to handle form submission

--- src/App.tsx
+++ src/App.tsx
@@ -69,27 +69,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 const App: React.FC = () => {
-  const [title, setTitle] = useState("");
-  const postData = {
-    title: "my title",
-    body: "hello man",
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files) {
+      const uploadedFile = files[0];
+      const formData = new FormData();
+      formData.append(uploadedFile.name, uploadedFile);
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((resp) => {
+          console.log(resp);
+        });
+    }
   };
-  useEffect(() => {
-    axios
-      .post("https://jsonplaceholder.typicode.com/posts", postData)
-      .then((resp) => {
-        console.log(resp);
-        setTitle(resp.data.title);
-      });
-  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>{title}</h1>
-      </header>
+    <div className="App" style={{ marginTop: "100px", marginLeft: "100px" }}>
+      <input type="file" name="myFile" onChange={handleFileChange} />
     </div>
   );
 };
 
 export default App;
+
+   
 

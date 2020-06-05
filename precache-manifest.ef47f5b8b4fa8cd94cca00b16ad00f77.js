656685a2fc7c89ddee489ccc6cbc652fb32b27db
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-05 17:01:36 +0000

    Deploy Waynexia888/ui-elements-react to github.com/Waynexia888/ui-elements-react.git:gh-pages

---
+++ precache-manifest.ef47f5b8b4fa8cd94cca00b16ad00f77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f088241226e6e38be2d2bfb148c558",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.13d50e9760448760e7c2.bundle.js"
+  },
+  {
+    "url": "runtime~main.13d50e9760448760e7c2.bundle.js"
+  },
+  {
+    "revision": "18205fe7af1e38f6bbbf",
+    "url": "static/css/main.b3dad4da.chunk.css"
+  },
+  {
+    "revision": "307864a6e9081ab6baa1",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.13d50e9760448760e7c2.bundle.js"
+  },
+  {
+    "url": "vendors~main.13d50e9760448760e7c2.bundle.js.LICENSE.txt"
+  }
+]);

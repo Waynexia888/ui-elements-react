b1fa53dac4a598ed93db2e29a5705f7cd3990639
Waynexia888 <qiwuwaynexia@gmail.com>
2020-06-04 22:12:07 -0700

    upload to github pages

---
+++ storybook-static/main.6a2d5991252a51b5574c.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1250:function(n,o,c){"use strict";c.r(o);c(1251),c(1283)},422:function(n,o,c){c(423),c(570),n.exports=c(1250)},489:function(n,o){}},[[422,1,2]]]);
---
+++ storybook-static/precache-manifest.5984e9c336318775c41089bc28b5a7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e859e23c64c6d5cab35aac686653e8b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.42d76325a6387d01ea7c.bundle.js"
+  },
+  {
+    "url": "runtime~main.42d76325a6387d01ea7c.bundle.js"
+  },
+  {
+    "revision": "31929743de991ce5d35a",
+    "url": "static/css/main.b3dad4da.chunk.css"
+  },
+  {
+    "revision": "5a14be62071e32c38e13",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.42d76325a6387d01ea7c.bundle.js"
+  },
+  {
+    "url": "vendors~main.42d76325a6387d01ea7c.bundle.js.LICENSE.txt"
+  }
+]);
